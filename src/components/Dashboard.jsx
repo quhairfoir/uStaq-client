8a23b6856336276bce9a04d7e839ee2a9fc89564
Maxim Nechaev <4max.nechaev@gmail.com>
2018-09-23 11:01:59 -0400

    Initial front-end set up

---
+++ src/components/Dashboard.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import {Row, Col, PageHeader} from 'react-bootstrap'
+
+const Dashboard = (props) => (
+  <Row>
+    <Col xs={12}>
+      <PageHeader>
+        uStaq <small>u learn</small>
+      </PageHeader>
+    </Col>
+  </Row>
+)
+
+export default Dashboard
