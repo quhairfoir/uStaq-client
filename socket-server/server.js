ffbd00eed8e31668d16d7cade4f29bc3f202dc25
Morag <moragstc@gmail.com>
2018-09-28 16:55:58 -0400

    correct socket.io dependency now

--- socket-server/server.js
+++ socket-server/server.js
@@ -1,6 +1,14 @@
-var express = require('express');
-var app = express();
+var express = require('express')
+var app = express()
 
-server = app.listen(3001, function(){
+
+server = app.listen(3001, function () {
     console.log('server is running on port 3001')
+});
+
+var socket = require('socket.io');
+io = socket(server);
+
+io.on('connection', (socket) => {
+    console.log(socket.id);
 });
