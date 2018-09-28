3654a4428938fa223f7d1e057eb7a2e55dd5ac47
Morag <moragstc@gmail.com>
2018-09-28 14:48:28 -0400

    base styling and elements added

--- src/components/QuizRoom.jsx
+++ src/components/QuizRoom.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-// import {Row, Col, PageHeader, Table} from 'react-bootstrap'
+import {Row, Grid, FormGroup, FormControl, ControlLabel, PageHeader, Well, Panel} from 'react-bootstrap'
 // import {Route, Switch, Link} from 'react-router-dom'
 
 
@@ -7,9 +7,30 @@ class QuizRoom extends React.Component {
 
   render() {
     return (
-      <div>
-        <h1>QUIZROOMMMMMM</h1>
-      </div>
+      <Grid>
+        <Row>
+          <PageHeader>
+            <small>The (mostly) educational trivia game</small>
+          </PageHeader>
+        </Row>
+        <Row>
+          <Panel>
+            <Panel.Heading>
+              <h2>Q: _________ are small fuzzy creatures.</h2>
+            </Panel.Heading>
+            <Panel.Body id="messageBox">
+            <p><strong>Dia</strong>: Sea Otters!</p>
+            <p><strong>Morag</strong>: Damn, I was going to guess that...</p>
+            </Panel.Body>
+            <Panel.Footer>
+              <FormGroup controlId="formControlsTextarea" className="quizTextArea">
+                <ControlLabel>Your answer...?</ControlLabel>
+                <FormControl componentClass="textarea" placeholder="..." />
+              </FormGroup>
+            </Panel.Footer>
+          </Panel>
+        </Row>
+      </Grid>
     )
   }
 }
