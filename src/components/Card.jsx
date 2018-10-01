58e08cef78d0461093fc55086a1fef5180d8e584
Maxim Nechaev <4max.nechaev@gmail.com>
2018-10-01 17:17:02 -0400

    CHanging carousel spacebar behavior

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,68 +1,87 @@
-import React, { Component } from 'react';
-import {Redirect, Route} from 'react-router-dom'
+import React, {Component} from 'react';
+import {Redirect} from 'react-router-dom'
 import {Button, Modal} from 'react-bootstrap'
 
 // Client-side model
 import Resource from '../models/resource'
+import CardCarousel from './CardCarousel'
+
+
 // import CardScroll from './CardScroll'
 const StackStore = Resource('stacks')
-
 
 class Card extends Component {
   constructor(props) {
     super(props)
 
-    this.handleShow = this.handleShow.bind(this);
-    this.handleHide = this.handleHide.bind(this);
+    this.toggleModal = this.toggleModal.bind(this);
+    this.toggleCard = this.toggleCard.bind(this);
+    this.keyHandle = this.keyHandle.bind(this);
 
     this.state = {
-      // stackId: (this.props.match.params.id || null),
-      stackId: this.props.location.pathname.split("/")[2] || null,
+      stackId: (this.props.match.params._id || null),
+      // stackId: this.props.location.pathname.split("/")[2] || null,
+      isFlipped: 'false',
       stack: {},
-      show: 'false',
+      isOpen: 'true',
       redirect: '/stacks'
     };
   }
 
-  handleShow() {
-    this.setState({ show: 'true' });
+  toggleModal = () => {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
   }
 
-  handleHide(){
-    this.setState({ show: 'false' });
+  toggleCard = () => {
+    this.setState({
+      isFlipped: !this.state.isFlipped,
+    });
   }
 
+  keyHandle(event) {
+    switch (event.keyCode) {
+      case 32:
+      this.toggleCard();
+      console.log("You pressed spacebar");
+        break;
+      case 39:
+      console.log("You pressed right arrow");
+        break;
+      case 37:
+      console.log("You pressed left arrow");
+        break;
+      case 27:
+      this.toggleModal();
+      console.log("You pressed escape");
+        break;
+      default:
+    }
+  }
 
-render() {
-  const showCard = () => {
-    console.log("Props here: ", this.props);
-    console.log("stackId here: ", this.state.stackId);
-      let item = this.props.stacks.find(target => target.id == this.state.stackId)
-      console.log("Item here: ", item);
+  render() {
+    const showCard = () => {
+      let item = this.props.stacks.find(target => target._id == this.state.stackId)
       if (!item) {
-        return <Redirect to={this.state.redirect} />
+        return <Redirect to={this.state.redirect}/>
       } else {
         return (
-
-        <div>
-
-          {item.description}
-
-        </div>
-
-      )
+          <Modal show={this.state.isOpen} onClose={this.toggleModal} onKeyDown={this.keyHandle} isFlipped={this.state.isFlipped} >
+            <Modal.Body>
+              <CardCarousel stack={item} isFlipped={this.state.isFlipped} />
+              <Button onClick={this.toggleModal} className="card-button">Close</Button>
+            </Modal.Body>
+          </Modal>
+        )
+      }
+    }
+    return (
+      <div>
+        {showCard()}
+      </div>
+    )
   }
-}
-  return (
-
-    <div>
-      {showCard()}
-
-    </div>
-  )
-}
-
-
 }
 
 export default Card
---
+++ src/components/CardCarousel.jsx
@@ -0,0 +1,35 @@
+// https://www.npmjs.com/package/react-responsive-carousel
+import React  from 'react';
+import ReactDOM from 'react-dom'
+import '../styles/carousel.css';
+
+import {Carousel} from 'react-responsive-carousel';
+
+const CardCarousel = (props) => (
+  <Carousel showArrows={true} infiniteLoop useKeyboardArrows showIndicators={false} showThumbs={false} width="100%">
+    {
+        props.stack.sentences.map((sentence, index) =>
+          <div className="container" key={index}>
+            <div className="row">
+              <div className={`info-card ${props.isFlipped ? 'flipped' : ''}`}>
+                <div className='front'>
+                  <p className="legend">Card front. {sentence.text.content}</p>
+                </div>
+                <div className='back'>
+                  <p className="legend">Card reverse. {sentence.text.content}</p>
+                </div>
+              </div>
+            </div>
+          </div>
+
+      )
+    }
+  </Carousel>
+
+)
+export default CardCarousel
+// ReactDOM.render(<CardCarousel />, document.querySelector('.demo-carousel'));
+
+// Don't forget to include the css in your page
+// <link rel="stylesheet" href="carousel.css"/>
+// Begin DemoSliderControls
