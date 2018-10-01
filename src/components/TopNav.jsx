7dbb0d7be1473ed1e74149ebddac9c609e87fdae
Morag <moragstc@gmail.com>
2018-10-01 18:02:43 -0400

    removed edit from nav

--- src/components/TopNav.jsx
+++ src/components/TopNav.jsx
@@ -84,20 +84,13 @@ class ModalNav extends React.Component {
             <Nav>
               <NavDropdown eventKey={1} title="Stacks" id="basic-nav-dropdown">
                 <MenuItem eventKey={1.1}><Link to={`/stacks`}>My Stacks</Link></MenuItem>
-                <MenuItem eventKey={1.2}>All Stacks</MenuItem>
                 <MenuItem divider />
-                <MenuItem eventKey={1.3}><Link to={`/edit`}>Edit</Link></MenuItem>
+                <MenuItem eventKey={1.2}>All Stacks</MenuItem>
+                
               </NavDropdown>
               <NavItem eventKey={2}>
                 <Link to={`/quizroom`}>Quiz</Link>
               </NavItem>
-              {/* <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
-                <MenuItem eventKey={3.1}>Action</MenuItem>
-                <MenuItem eventKey={3.2}>Another action</MenuItem>
-                <MenuItem eventKey={3.3}>Something else here</MenuItem>
-                <MenuItem divider />
-                <MenuItem eventKey={3.3}>Separated link</MenuItem>
-              </NavDropdown> */}
             </Nav>
             <Nav pullRight>
               <NavItem eventKey={1} >
